2ac3314c3a62ac820228ec7068806bb06c9455d6
trungnhat06072206-alt <phamtuandibar@gmail.com>
2025-10-05 21:16:18 +0700

    Add files via upload

---
+++ 2.6.py
@@ -0,0 +1,6 @@
+# Bài 1: Làm việc với chuỗi
+
+my_string = "lap trinh python vui va de hieu"
+print("Nội dung chuỗi:", my_string)
+print("Chiều dài chuỗi:", len(my_string))
+print("Chuỗi viết hoa:", my_string.upper())

8581b5a2b2681a8d0f2823446f85998689235406
trungnhat06072206-alt <phamtuandibar@gmail.com>
2025-10-05 21:18:12 +0700

    Delete 2.6.py

--- 2.6.py
+++
@@ -1,6 +0,0 @@
-# Bài 1: Làm việc với chuỗi
-
-my_string = "lap trinh python vui va de hieu"
-print("Nội dung chuỗi:", my_string)
-print("Chiều dài chuỗi:", len(my_string))
-print("Chuỗi viết hoa:", my_string.upper())
